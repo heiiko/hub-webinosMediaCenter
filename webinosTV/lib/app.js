e8b2f87939576e51193d312b51ded4ff346d6534
Felix-Johannes Jendrusch <felixjendrusch@googlemail.com>
2013-08-16 11:58:27 +0200

    Remove device manager example

--- webinosTV/lib/app.js
+++ webinosTV/lib/app.js
@@ -9,10 +9,5 @@ var WebinosController = require('./controller/app_controller.js');
 
 $(document).ready(function() {
   var manager = new DeviceManager(30000, 60000);
-  manager.filter('.isFound').onValue(function (event) {
-    console.log('New device found!', event.device().address());
-    return event.device();
-  });
-
   new WebinosController(manager, view);
 });
