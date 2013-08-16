f0d11fa6a5cf7e1a81a35d3ad4cec109452ecd3f
Felix-Johannes Jendrusch <felixjendrusch@googlemail.com>
2013-08-16 11:54:24 +0200

    Require devices to have a Device Status API

--- webinosTV/lib/app.js
+++ webinosTV/lib/app.js
@@ -9,20 +9,10 @@ var WebinosController = require('./controller/app_controller.js');
 
 $(document).ready(function() {
   var manager = new DeviceManager(30000, 60000);
-
-  new WebinosController(manager, view);
-
-  manager.filter('.isFound').flatMap(function (event) {
+  manager.filter('.isFound').onValue(function (event) {
     console.log('New device found!', event.device().address());
     return event.device();
-  }).filter('.isAvailable').map(function (event) {
-    console.log('New service found!', event.service().address(), event.service().id());
-    return event.service();
-  }).flatMap(function (service) {
-    return Bacon.fromPromise(service.bindService());
-  }).onValue(function (service) {
-    console.log('Service bound!', service.address(), service.id());
-    service.unbindService();
   });
 
+  new WebinosController(manager, view);
 });
