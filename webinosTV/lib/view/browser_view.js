6bb09b60a8705e874840b41ab6cc5559748a30a7
Vito Macchia <vito.macchia@gmail.com>
2013-09-04 16:51:56 +0200

    Created selection arrow in select media for large screens

--- webinosTV/lib/view/browser_view.js
+++ webinosTV/lib/view/browser_view.js
@@ -7,12 +7,12 @@ var util = require('util');
 
 $(document).ready(function() {
   //$('.albumToggleIcon').click(function() {
-   // if ($(this).parent("div").next('ul').is(":visible")) {
-   //   $(this).attr("src", "images/arrow_big_down.svg");
-   // } else {
-   //   $(this).attr("src", "images/arrow_big_up.svg");
-   // }
-   // $(this).parent().next('ul').slideToggle(250, function() { contentScroll.refresh(); });
+  // if ($(this).parent("div").next('ul').is(":visible")) {
+  //   $(this).attr("src", "images/arrow_big_down.svg");
+  // } else {
+  //   $(this).attr("src", "images/arrow_big_up.svg");
+  // }
+  // $(this).parent().next('ul').slideToggle(250, function() { contentScroll.refresh(); });
   //});
   //$('.albumsonglist > li > img, #contentlist > li > img').click(function() {
   //  var src = ($(this).attr('src') === 'images/add.svg')
@@ -51,19 +51,19 @@ function ListView(items, selection, list, wrapper, fadeout) {
   var self = this;
   var scroll = undefined;
 
-  this.refresh = function () {
+  this.refresh = function() {
     if ($(list).children().length > 0) {
       if (typeof scroll === 'undefined') {
         scroll = new IScroll(wrapper, {snap: 'li', momentum: false});
-        scroll.on('scrollEnd', function(){
-          if(scroll.y >= 0){
+        scroll.on('scrollEnd', function() {
+          if (scroll.y >= 0) {
             //$(fadeout + 'topfadeout').hide();
-          }else{
+          } else {
             //$(fadeout + 'topfadeout').show();
           }
-          if(scroll.y <= ($(wrapper).height() - $(list).height())){
+          if (scroll.y <= ($(wrapper).height() - $(list).height())) {
             //$(fadeout + 'bottomfadeout').hide();
-          }else{
+          } else {
             //$(fadeout + 'bottomfadeout').show();
           }
         });
@@ -72,11 +72,11 @@ function ListView(items, selection, list, wrapper, fadeout) {
     }
   };
 
-  items.onValue(function (items) {
+  items.onValue(function(items) {
     var $list = $(list);
     $list.empty();
 
-    _.each(items, function (item) {
+    _.each(items, function(item) {
       var $item = $(self.htmlify(item));
       var id = self.identify(item);
       $item.data('id', id);
@@ -86,24 +86,24 @@ function ListView(items, selection, list, wrapper, fadeout) {
     self.refresh();
   });
 
-  selection.apply(items.map(function (items) {
-    return function (selection) {
-      return _.ointersection(selection, _.map(items, function (item) {
+  selection.apply(items.map(function(items) {
+    return function(selection) {
+      return _.ointersection(selection, _.map(items, function(item) {
         return self.identify(item);
       }));
     };
   }));
 
-  selection.apply($(list).asEventStream('click').map(function (event) {
-    return function (selection) {
+  selection.apply($(list).asEventStream('click').map(function(event) {
+    return function(selection) {
       var $item = $(event.target).closest('li');
       var id = $item.data('id');
       return (_.ocontains(selection, id) ? _.odifference : _.ounion)(selection, [id]);
     };
   }));
 
-  selection.onValue(function (selection) {
-    $('li', list).each(function () {
+  selection.onValue(function(selection) {
+    $('li', list).each(function() {
       var $item = $(this);
       var id = $item.data('id');
       $item.toggleClass('selected', _.ocontains(selection, id));
@@ -113,14 +113,14 @@ function ListView(items, selection, list, wrapper, fadeout) {
 
 util.inherits(DeviceListView, ListView);
 function DeviceListView(items, selection, list, wrapper, fadeout) {
-  this.htmlify = function (device) {
+  this.htmlify = function(device) {
     return '<li class="device"><div class="device-image type-' + device.type() + '"></div><div class="device-name">' + device.address() + '</div><div class="device-type">' + device.type().charAt(0).toUpperCase() + device.type().slice(1) + '</div></li>';
   };
 
-  this.identify = function (device) {
+  this.identify = function(device) {
     return {
-    	address: device.address(),
-    	type: device.type()
+      address: device.address(),
+      type: device.type()
     };
   };
 
@@ -129,59 +129,81 @@ function DeviceListView(items, selection, list, wrapper, fadeout) {
 
 util.inherits(SourceListView, DeviceListView);
 function SourceListView(viewModel) {
-	DeviceListView.call(this, viewModel.sources(), viewModel.selectedSources(), '#sourcelist', '#sourcewrapper', '#source');
-  
-	viewModel.selectedSources().onValue(function (selection) {
-    	if(selection.length == 1) {
-    		var device = selection[0];
-    		$('#selected-source').attr('src', 'images/' + device.type + '-selected.svg');
-    		$('#selected-source-name').html(device.address);
-    		$('#selected-source-intro').html('You can select media from');
-    		
-    		$('#wrapper-selected-source').removeClass('header-active');
-    		$('#wrapper-selected-target').addClass('header-active');
-    		
-    		$('#current-source-logo').attr('src', 'images/' + device.type + '.svg');
-    		$('#current-source-name').html(device.address);
-    	}
-    	else if(selection.length == 0) {
-    		$('#selected-source').attr('src', '');
-    		$('#selected-source-name').html('');
-    		$('#selected-source-intro').html('');
-    		
-    		$('#wrapper-selected-source').addClass('header-active');
-    		$('#wrapper-selected-target').removeClass('header-active');
-    		
-    		$('#current-source-logo').attr('src', '');
-    		$('#current-source-name').html('Source device');
-    	}
-    	else {
-    		$('#current-source-logo').attr('src', '');
-    		$('#current-source-name').html(selection.length + ' Source devices');
-    	}
-  	});
+  DeviceListView.call(this, viewModel.sources(), viewModel.selectedSources(), '#sourcelist', '#sourcewrapper', '#source');
+
+  viewModel.selectedSources().onValue(function(selection) {
+    if (selection.length === 1) {
+      var device = selection[0];
+      $('#selected-source').attr('src', 'images/' + device.type + '-selected.svg');
+      $('#selected-source-name').html(device.address);
+      $('#selected-source-intro').html('You can select media from');
+
+      $('#wrapper-selected-source').removeClass('header-active');
+      $('#wrapper-selected-target').addClass('header-active');
+
+      $('#current-source-logo').attr('src', 'images/' + device.type + '.svg');
+      $('#current-source-name').html(device.address);
+    }
+    else if (selection.length === 0) {
+      $('#selected-source').attr('src', '');
+      $('#selected-source-name').html('');
+      $('#selected-source-intro').html('');
+
+      $('#wrapper-selected-source').addClass('header-active');
+      $('#wrapper-selected-target').removeClass('header-active');
+
+      $('#current-source-logo').attr('src', '');
+      $('#current-source-name').html('Source device');
+    }
+    else {
+      $('#current-source-logo').attr('src', '');
+      $('#current-source-name').html(selection.length + ' Source devices');
+    }
+  });
 }
 
 util.inherits(CategoryListView, ListView);
 function CategoryListView(viewModel) {
-  this.htmlify = function (category) {
+  this.htmlify = function(category) {
     return '<li class="category"><img class="category-image" src="' + category.image + '"><div class="category-name">' + category.title + '</div></li>';
   };
 
-  this.identify = function (category) {
+  this.identify = function(category) {
     return category.id;
   };
 
   ListView.call(this, viewModel.categories(), viewModel.selectedCategories(), '#categorylist', '#categorywrapper', '#category');
 }
 
+///VITO
+//util.inherits(CategoryContentListView, ListView);
+//function CategoryContentListView(viewModel) {
+//  console.warn("DAFUQ - CategoryContentListView", viewModel);
+//  this.htmlify = function(category) {
+////    return '<li class="category-content arrow_box"><div class="content-wrapper">Content for ' + category.title + '</div></li>';
+//    return '<li class="category-content arrow_box"><div class="content-wrapper"><ul class="content-list"><li>Loading ' + category.title + '</li></ul></div></li>';
+//  };
+//
+//  this.identify = function(category) {
+//    return category.id;
+//  };
+//
+//  ListView.call(this, viewModel.categories(), viewModel.selectedCategories(), '#categorycontentlist', '#categorycontentwrapper', '#categorycontent');
+//}
+
+///VITO
+
 util.inherits(ContentListView, ListView);
 function ContentListView(viewModel) {
-  this.htmlify = function (value) {
-    return '<li><p>'+((typeof value.item.type=='string' && value.item.type.toLowerCase().indexOf("image")==0)?('<img src="' + value.item.thumbnailURIs[0] + '">'):(value.item.title)) + '</p><img src="images/add.svg"></li>';
+  console.warn("DAFUQ ContentListView", viewModel);
+  this.htmlify = function(value) {
+    console.warn("DAFUQ III", value);
+    return '<li>Cthulhu Fhtagn!< /li>';
+    //TODO define .media_item scss
+//    return '<li class="media_item"><p>' + ((typeof value.item.type === 'string' && value.item.type.toLowerCase().indexOf("image") === 0) ? ('<img src="' + value.item.thumbnailURIs[0] + '">') : (value.item.title)) + '</p><img src="images/add.svg"></li>';
   };
 
-  this.identify = function (value) {
+  this.identify = function(value) {
     return {
       source: value.source.address(),
       item: {
@@ -190,7 +212,6 @@ function ContentListView(viewModel) {
       }
     };
   };
-
   ListView.call(this, viewModel.content(), viewModel.selectedContent(), '#contentlist', '#contentwrapper', '#content');
 }
 
@@ -198,28 +219,29 @@ util.inherits(TargetListView, DeviceListView);
 function TargetListView(viewModel) {
   DeviceListView.call(this, viewModel.targets(), viewModel.selectedTargets(), '#targetlist', '#targetwrapper', '#target');
 
-  viewModel.selectedTargets().onValue(function (selection) {
-    	if(selection.length == 1) {
-    		var device = selection[0];
-    		
-    		$('#current-target-logo').attr('src', 'images/' + device.type + '.svg');
-    		$('#current-target-name').html(device.address);
-    	}
-    	else if(selection.length == 0) {
-    		$('#current-target-logo').attr('src', '');
-    		$('#current-target-name').html('Target device');
-    	}
-    	else {
-    		$('#current-target-logo').attr('src', '');
-    		$('#current-target-name').html(selection.length + ' Target devices');
-    	}
-  	});
+  viewModel.selectedTargets().onValue(function(selection) {
+    if (selection.length === 1) {
+      var device = selection[0];
+
+      $('#current-target-logo').attr('src', 'images/' + device.type + '.svg');
+      $('#current-target-name').html(device.address);
+    }
+    else if (selection.length === 0) {
+      $('#current-target-logo').attr('src', '');
+      $('#current-target-name').html('Target device');
+    }
+    else {
+      $('#current-target-logo').attr('src', '');
+      $('#current-target-name').html(selection.length + ' Target devices');
+    }
+  });
 }
 
 function BrowserView(viewModel) {
   var sourceListView = new SourceListView(viewModel);
   var categoryListView = new CategoryListView(viewModel);
-  //var contentListView = new ContentListView(viewModel);
+//  var categoryContentListView = new CategoryContentListView(viewModel);
+  var contentListView = new ContentListView(viewModel);
   var targetListView = new TargetListView(viewModel);
 
   //viewModel.play().plug($('#play').asEventStream('click').map());
--- webinosTV/lib/view/main_view.js
+++ webinosTV/lib/view/main_view.js
@@ -3,36 +3,108 @@ var activemenu = '#menu-device';
 var currentpage = '#container-device';
 
 $(document).ready(function() {
-	init();
+  init();
 });
 
 function init() {
-	$(activemenu).addClass('menu-active');
-	$(currentpage).addClass('subpage-current');
-	
-	$('#menu-device').on('click', function(){ gotoPageById('#container-device'); setActiveMenu('#menu-device');});
-    $('#menu-media').on('click', function(){ gotoPageById('#container-media'); setActiveMenu('#menu-media');});
-    $('#menu-queue').on('click', function(){ gotoPageById('#container-queue'); setActiveMenu('#menu-queue');});
+  $(activemenu).addClass('menu-active');
+  $(currentpage).addClass('subpage-current');
 
-	if (window.location.hash){
-		gotoPageById(window.location.hash);
-	}
+  $('#menu-device').on('click', function() {
+    gotoPageById('#container-device');
+    setActiveMenu('#menu-device');
+  });
+  $('#menu-media').on('click', function() {
+    gotoPageById('#container-media');
+    setActiveMenu('#menu-media');
+
+    //Selection Arrow logic
+    function getClassStyles(parentElem, selector, stylePropertyName) {
+      var elemstr = '<div ' + selector + '></div>';
+      var $elem = $(elemstr).hide().appendTo(parentElem);
+      var val = $elem.css(stylePropertyName);
+      $elem.remove();
+      return val;
+    }
+
+    var moveCategorySelectionArrow = function(yposition) {
+      $('#sel-arrows-style').html('@media screen and (min-width: 1200px){.arrow_box:after{top:' + yposition + 'px}.arrow_box:before{top:' + yposition + 'px}.yposition-reminder{top:' + yposition + 'px;display:none}}');
+    };
+    var selectMediaCategories = $('li.category');
+    //var selectMediaContents = $('#contentlist');//$('#categorycontentlist li.category-content.arrow_box');
+    var clheight = $('li.category')[0].clientHeight;
+    var clpadding = parseInt($($('li.category')[0]).css('padding'));
+    var imgHeight = $('img.category-image')[0].clientHeight;
+
+    for (var i = 0; i < selectMediaCategories.length; i++) {
+      (function(i) {
+        $(selectMediaCategories[i]).on('click', function() {
+          var ypos = parseFloat(getClassStyles('body', 'class="yposition-reminder"', 'top'));
+          var newYpos = i * clheight + imgHeight / 2 + clpadding;
+          moveCategorySelectionArrow(newYpos);
+          if (ypos === newYpos && $('#contentlist').hasClass('arrow_box')) {
+            $('#contentlist').removeClass('arrow_box');
+          }
+          else {
+            $('#contentlist').addClass('arrow_box');
+          }
+        });
+      })(i);
+    }
+
+    /*
+     //Selection Arrow logic
+     var moveCategorySelectionArrow = function(height) {
+     $('#sel-arrows-style').html('@media screen and (min-width: 1200px){.arrow_box:after{top:' + height + 'px}.arrow_box:before{top:' + height + 'px}}');
+     };
+     var selectMediaCategories = $('li.category');
+     var selectMediaContents = $('#categorycontentlist li.category-content.arrow_box');
+     var clheight = $('li.category')[0].clientHeight;
+     var clpadding = parseInt($($('li.category')[0]).css('padding'));
+     var imgHeight = $('img.category-image')[0].clientHeight;
+     console.warn("DAFUQ 2", selectMediaCategories, selectMediaContents);
+
+     for (var i = 0; i < selectMediaCategories.length; i++) {
+     (function(i) {
+     $(selectMediaCategories[i]).on('click', function() {
+     selectMediaContents.each(function(j) {
+     if (i === j) {
+     moveCategorySelectionArrow(i * clheight + imgHeight / 2 + clpadding);
+     $(this).toggle();
+     }
+     else {
+     $(this).hide();
+     }
+     });
+     });
+     })(i);
+     }
+     */
+  });
+  $('#menu-queue').on('click', function() {
+    gotoPageById('#container-queue');
+    setActiveMenu('#menu-queue');
+  });
+
+  if (window.location.hash) {
+    gotoPageById(window.location.hash);
+  }
 }
 
 function gotoPageById(id) {
-    if (currentpage != id) {
-        var $currPage = $(currentpage);
-        $(id).addClass('subpage-current');
-		$currPage.removeClass('subpage-current');
-        currentpage = id;
-    }
+  if (currentpage !== id) {
+    var $currPage = $(currentpage);
+    $(id).addClass('subpage-current');
+    $currPage.removeClass('subpage-current');
+    currentpage = id;
+  }
 }
 
 function setActiveMenu(id) {
-    if (activemenu != id) {
-        var $currMenu = $(activemenu);
-        $(id).addClass('menu-active');
-		$currMenu.removeClass('menu-active');
-        activemenu = id;
-    }
+  if (activemenu !== id) {
+    var $currMenu = $(activemenu);
+    $(id).addClass('menu-active');
+    $currMenu.removeClass('menu-active');
+    activemenu = id;
+  }
 }
