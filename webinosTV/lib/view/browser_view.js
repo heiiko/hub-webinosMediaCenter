3d9d613419e25648283eaa3d7245be197a7430ba
hdsruell <heiko@desruelle.be>
2013-09-05 12:53:17 +0200

    model selection grouping

--- webinosTV/lib/view/browser_view.js
+++ webinosTV/lib/view/browser_view.js
@@ -79,11 +79,58 @@ function ListView(items, selection, list, wrapper, fadeout) {
     };
   }));
 
-  selection.onValue(function(selection) {
-    $('li', list).each(function() {
+  selection.onValue(function (selection) {
+  	console.log(selection);
+    $('li', list).each(function () {
       var $item = $(this);
       var id = $item.data('id');
-      $item.toggleClass('selected', _.ocontains(selection, id));
+	  $item.toggleClass('selected', _.ocontains(selection, id));
+	  
+      if($item.hasClass('source')){
+        if (selection.length == 1) {
+      		var device = selection[0];
+      		$('#selected-source').attr('src', 'images/' + device.type + '-selected.svg');
+      		$('#selected-source-name').html(device.address);
+      		$('#selected-source-intro').html('You can select media from');
+
+      		$('#wrapper-selected-source').removeClass('header-active');
+      		$('#wrapper-selected-target').addClass('header-active');
+
+  	        $('#current-source-logo').attr('src', 'images/' + device.type + '.svg');
+      		$('#current-source-name').html(device.address);
+    	}
+    	else if (selection.length == 0) {
+      		$('#selected-source').attr('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==');
+      		$('#selected-source-name').html('');
+      		$('#selected-source-intro').html('');
+   	        $('#wrapper-selected-source').addClass('header-active');
+      		$('#wrapper-selected-target').removeClass('header-active');
+
+		
+	        $('#current-source-logo').attr('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==');
+      		$('#current-source-name').html('Source device');
+    	}
+    	else {
+      		$('#current-source-logo').attr('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==');
+      		$('#current-source-name').html(selection.length + ' Source devices');
+    	}
+      }
+      else if($item.hasClass('target')){
+    	if(selection.length == 1) {
+        	var device = selection[0];
+        
+        	$('#current-target-logo').attr('src', 'images/' + device.type + '.svg');
+        	$('#current-target-name').html(device.address);
+      	}
+      	else if(selection.length == 0) {
+        	$('#current-target-logo').attr('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==');
+        	$('#current-target-name').html('Target device');
+      	}
+      	else {
+        	$('#current-target-logo').attr('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==');
+        	$('#current-target-name').html(selection.length + ' Target devices');
+      	}
+      }
     });
   });
 }
@@ -91,7 +138,7 @@ function ListView(items, selection, list, wrapper, fadeout) {
 util.inherits(SourceListView, ListView);
 function SourceListView(viewModel) {
   this.htmlify = function(device) {
-    return '<li class="device"><div class="device-image type-' + device.type() + '"></div><div class="device-name">' + device.address() + '</div><div class="device-type">' + device.type().charAt(0).toUpperCase() + device.type().slice(1) + '</div></li>';
+    return '<li class="device source"><div class="device-image type-' + device.type() + '"></div><div class="device-name">' + device.address() + '</div><div class="device-type">' + device.type().charAt(0).toUpperCase() + device.type().slice(1) + '</div></li>';
   };
 
   this.identify = function(device) {
@@ -100,36 +147,6 @@ function SourceListView(viewModel) {
       type: device.type()
     };
   };
-
-  viewModel.selectedSources().onValue(function(selection) {
-    if (selection.length == 1) {
-      var device = selection[0];
-      $('#selected-source').attr('src', 'images/' + device.type + '-selected.svg');
-      $('#selected-source-name').html(device.address);
-      $('#selected-source-intro').html('You can select media from');
-
-      $('#wrapper-selected-source').removeClass('header-active');
-      $('#wrapper-selected-target').addClass('header-active');
-
-      $('#current-source-logo').attr('src', 'images/' + device.type + '.svg');
-      $('#current-source-name').html(device.address);
-    }
-    else if (selection.length == 0) {
-      $('#selected-source').attr('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==');
-      $('#selected-source-name').html('');
-      $('#selected-source-intro').html('');
-
-      $('#wrapper-selected-source').addClass('header-active');
-      $('#wrapper-selected-target').removeClass('header-active');
-
-      $('#current-source-logo').attr('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==');
-      $('#current-source-name').html('Source device');
-    }
-    else {
-      $('#current-source-logo').attr('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==');
-      $('#current-source-name').html(selection.length + ' Source devices');
-    }
-  });
 
   ListView.call(this, viewModel.sources(), viewModel.selectedSources(), '#sourcelist', '#sourcewrapper', '#source');
 }
@@ -191,7 +208,7 @@ function ContentListView(viewModel) {
 util.inherits(TargetListView, ListView);
 function TargetListView(viewModel) {
   this.htmlify = function(device) {
-    return '<li class="device"><div class="device-image type-' + device.type() + '"></div><div class="device-name">' + device.address() + '</div><div class="device-type">' + device.type().charAt(0).toUpperCase() + device.type().slice(1) + '</div></li>';
+    return '<li class="device target"><div class="device-image type-' + device.type() + '"></div><div class="device-name">' + device.address() + '</div><div class="device-type">' + device.type().charAt(0).toUpperCase() + device.type().slice(1) + '</div></li>';
   };
 
   this.identify = function(device) {
