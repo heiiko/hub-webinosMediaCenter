39f3a6dcae27057452a5f1dfba0cdd6d7aee0514
hdsruell <heiko@desruelle.be>
2013-10-23 11:47:05 +0200

    Reconnecting websockets

--- webinosTV/src/vendor/webinos.js
+++ webinosTV/src/vendor/webinos.js
@@ -2103,6 +2103,136 @@ if (typeof _webinos === "undefined") {
 (function () {
     var channel = null;
 
+	function ReconnectingWebSocket(url, protocols) {
+		protocols = protocols || [];
+
+		// These can be altered by calling code.
+		this.debug = true;
+		this.reconnectInterval = 1000;
+		this.timeoutInterval = 2000;
+
+		var self = this;
+		var ws;
+		var forcedClose = false;
+		var timedOut = false;
+	
+		this.url = url;
+		this.protocols = protocols;
+		this.readyState = WebSocket.CONNECTING;
+		this.URL = url; // Public API
+
+		this.onopen = function(event) {
+		};
+
+		this.onclose = function(event) {
+		};
+
+		this.onconnecting = function(event) {
+		};
+
+		this.onmessage = function(event) {
+		};
+
+		this.onerror = function(event) {
+		};
+
+		function connect(reconnectAttempt) {
+			ws = new WebSocket(url, protocols);
+		
+			self.onconnecting();
+			if (self.debug || ReconnectingWebSocket.debugAll) {
+				console.debug('ReconnectingWebSocket', 'attempt-connect', url);
+			}
+		
+			var localWs = ws;
+			var timeout = setTimeout(function() {
+				if (self.debug || ReconnectingWebSocket.debugAll) {
+					console.debug('ReconnectingWebSocket', 'connection-timeout', url);
+				}
+				timedOut = true;
+				localWs.close();
+				timedOut = false;
+			}, self.timeoutInterval);
+		
+			ws.onopen = function(event) {
+				clearTimeout(timeout);
+				if (self.debug || ReconnectingWebSocket.debugAll) {
+					console.debug('ReconnectingWebSocket', 'onopen', url);
+				}
+				self.readyState = WebSocket.OPEN;
+				reconnectAttempt = false;
+				self.onopen(event);
+			};
+		
+			ws.onclose = function(event) {
+				clearTimeout(timeout);
+				ws = null;
+				if (forcedClose) {
+					self.readyState = WebSocket.CLOSED;
+					self.onclose(event);
+				} else {
+					self.readyState = WebSocket.CONNECTING;
+					self.onconnecting();
+					if (!reconnectAttempt && !timedOut) {
+						if (self.debug || ReconnectingWebSocket.debugAll) {
+							console.debug('ReconnectingWebSocket', 'onclose', url);
+						}
+						self.onclose(event);
+					}
+					setTimeout(function() {
+						connect(true);
+					}, self.reconnectInterval);
+				}
+			};
+			ws.onmessage = function(event) {
+				if (self.debug || ReconnectingWebSocket.debugAll) {
+					console.debug('ReconnectingWebSocket', 'onmessage', url, event.data);
+				}
+				self.onmessage(event);
+			};
+			ws.onerror = function(event) {
+				if (self.debug || ReconnectingWebSocket.debugAll) {
+					console.debug('ReconnectingWebSocket', 'onerror', url, event);
+				}
+				self.onerror(event);
+			};
+		}
+		connect(url);
+
+		this.send = function(data) {
+			if (ws) {
+				if (self.debug || ReconnectingWebSocket.debugAll) {
+					console.debug('ReconnectingWebSocket', 'send', url, data);
+				}
+				return ws.send(data);
+			} else {
+				throw 'INVALID_STATE_ERR : Pausing to reconnect websocket';
+			}
+		};
+
+		this.close = function() {
+			if (ws) {
+				forcedClose = true;
+				ws.close();
+			}
+		};
+
+		/**
+		 * Additional public API method to refresh the connection if still open (close, re-open).
+		 * For example, if the app suspects bad data / missed heart beats, it can try to refresh.
+		 */
+		this.refresh = function() {
+			if (ws) {
+				ws.close();
+			}
+		};
+	}
+
+	/**
+	 * Setting this to true is the equivalent of setting all instances of ReconnectingWebSocket.debug to true.
+	 */
+	ReconnectingWebSocket.debugAll = false;
+
     /**
      * Creates the socket communication channel
      * for a locally hosted websocket server at port 8080
@@ -2166,7 +2296,8 @@ if (typeof _webinos === "undefined") {
             // We are ready to make the connection.
 
             // Get the correct websocket object.
-            var ws = window.WebSocket || window.MozWebSocket;
+            //var ws = window.WebSocket || window.MozWebSocket;
+            var ws = ReconnectingWebSocket;
             try {
                 channel = new ws ("ws://" + hostname + ":" + port);
             } catch (err) { // Websockets are not available for this browser. We need to investigate in order to support it.
